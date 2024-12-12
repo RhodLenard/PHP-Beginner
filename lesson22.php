938857635af0b07b84fc72cc4bbe17f7ebf65fce
RhodLenard <rdelasnieves2023@student.nbscollege.edu.ph>
2024-12-13 00:39:27 +0800

    M

--- lesson22.php
+++ lesson22.php
@@ -1,14 +1,10 @@
 <?php
   include("database.php");
+
+  $sql = "INSERT INTO users (user, password)
+  VALUES ('Patrick', 'Pat102')";
+
+  mysqli_query($conn, $sql);
+
+  mysqli_close($conn);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-    Hello
-</body>
-</html>
